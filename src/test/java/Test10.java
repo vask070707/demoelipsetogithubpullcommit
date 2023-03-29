d823c09518d90268f192fc6ff4a9ad103fb0c5aa
vas070707 <vas070707@gmail.com>
2023-03-29 21:29:29 +0100

    hi hema sir, i just uploaded program 10, please check and comment me in github

---
+++ src/test/java/Test10.java
@@ -0,0 +1,35 @@
+// PROGRAM 10 : Objective is to check whether its a secured URL or not. for this if the url starts with https its a secured else unsecured url
+// get current url gets the url of the webpage which is already opened.
+
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test10 {
+
+	public static void main(String[] args) throws Exception {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		//2.SWD code to launch IE browser
+	RemoteWebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("http://www.google.co.in"); // it will launch the page in the browser
+		Thread.sleep(5000);
+		String x = driver.getCurrentUrl();// get current url gets the url of the webpage which is already opened
+		
+		if(x.startsWith("https"))
+		{
+		System.out.println("Secured site");
+		}
+		else
+		{
+			System.out.println("Unsecured site");
+		}
+		driver.close();
+
+	} }
